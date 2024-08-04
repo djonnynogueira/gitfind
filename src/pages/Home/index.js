316b629aa00ddbd37a543d5dea291bb9fe100644
Djonny Nogueira <djonnynogueira@gmail.com>
2024-08-04 10:32:39 -0300

    Criação profile.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,6 @@
 import { Header } from '../../components/Header';
 import background from '../../assets/background.png';
-
 import './styles_home.css';
-
-
 
 function App() {
   return (
@@ -12,9 +9,22 @@ function App() {
       <div className='conteudo'>
         <img src={background} className='background' alt='background app' />
         <div className='info'>
-          <input name='usuario' placeholder='@username' />
-          <button>Buscar</button>          
-          </div>
+          <div>
+            <input name='usuario' placeholder='@username' />
+            <button>Buscar</button>
+          </div>       
+            <div className='perfil'>
+              <img src='https://avatars.githubusercontent.com/u/13878534?v=4'
+                className='profile' alt='Imagem do perfil'
+              />
+              <div>
+                <h3>Djonny Nogueira</h3>
+                <span>Descrição</span>
+                <p>Descrição</p>
+              </div>
+            </div>      
+          <hr />
+        </div>
       </div>
     </div>
   );
