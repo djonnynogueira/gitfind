d565d153617719ad86321578e4fdfa4985eb967e
Djonny Nogueira <djonnynogueira@gmail.com>
2024-08-04 22:38:06 -0300

    Criação da paginação da lista dos repositórios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,31 +1,54 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Header } from '../../components/Header';
 import background from '../../assets/background.png';
 import './styles_home.css';
 import { ItemList } from '../../components/ItemList';
+import { Pagination } from '../../components/Pagination';
 
 function App() {
   const [user, setUser] = useState("")
   const [currentUser, setCurrentUser] = useState(null)
   const [repos, setRepos] = useState(null)
 
+  const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5);
+  const [currentItems, setCurrentItems] = useState([]);
+
   const handleGetData = async () => {
     const userData = await fetch(`https://api.github.com/users/${user}`);
     const newUser = await userData.json();
 
+
     // Dados usuários
     if (newUser.name) {
       const { avatar_url, name, bio, login } = newUser;
-      setCurrentUser({ avatar_url, name, bio, login})
+      setCurrentUser({ avatar_url, name, bio, login })
 
       const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
       const newRepos = await reposData.json();
+      setItems(newRepos);
 
       if (newRepos.length) {
-        setRepos(newRepos)
+        setRepos(newRepos);
+        setItems(newRepos);
+        setCurrentPage(1);
       }
     }
   }
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+
+  useEffect(() => {
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    setCurrentItems(items.slice(indexOfFirstItem, indexOfLastItem));
+  }, [currentPage, items, itemsPerPage]);
+
+  // Muda a página
+  const paginate = pageNumber => setCurrentPage(pageNumber);
 
 
   return (
@@ -61,14 +84,29 @@ function App() {
           {repos?.length ? (
             <div>
               <h4 className='repositorio'>Repositórios</h4>
-              {repos.map(repo => (
-                <ItemList title={repo.name} description={repo.description} />
+              {currentItems.map(repo => (
+                <ItemList key={repo.id} title={repo.name} description={repo.description} />
               ))}
+
             </div>
           ) : null}
+         <div>
+          
 
+            {items.length > 0 && (
+              <Pagination
+              itemsPerPage={itemsPerPage}
+              totalItems={items.length}
+              paginate={paginate}
+              currentPage={currentPage}
+              />
+            )}
+            </div>
+         
         </div>
+
       </div>
+
     </div>
   );
 }
