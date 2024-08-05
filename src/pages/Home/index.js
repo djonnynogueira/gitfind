d18a085ee9c82fe3d3335409d051ff364fd5e3cb
Djonny Nogueira <djonnynogueira@gmail.com>
2024-08-04 21:29:04 -0300

    Requisicao a api com fetch

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import { Header } from '../../components/Header';
 import background from '../../assets/background.png';
 import './styles_home.css';
 import { ItemList } from '../../components/ItemList';
 
 function App() {
+  const [user, setUser] = useState("")
+  const [currentUser, setCurrentUser] = useState(null)
+  const [repos, setRepos] = useState(null)
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    // Dados usuários
+    if (newUser.name) {
+      const { avatar_url, name, bio, login } = newUser;
+      setCurrentUser({ avatar_url, name, bio, login})
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if (newRepos.length) {
+        setRepos(newRepos)
+      }
+    }
+  }
+
+
   return (
     <div className="App">
       <Header />
@@ -11,30 +35,37 @@ function App() {
         <img src={background} className='background' alt='background app' />
         <div className='info'>
           <div>
-            <input name='usuario' placeholder='@username' />
-            <button>Buscar</button>
-          </div>   
+            <input
+              name='usuario'
+              value={user}
+              onChange={event => setUser(event.target.value)}
+              placeholder='@username' />
+            <button onClick={handleGetData}>Buscar</button>
+          </div>
 
+          {currentUser?.name ? (<>
             <div className='perfil'>
-              <img src='https://avatars.githubusercontent.com/u/13878534?v=4'
+              <img src={currentUser.avatar_url}
                 className='profile' alt='Imagem do perfil'
               />
               <div>
-                <h3>Djonny Nogueira</h3>
-                <span>Descrição</span>
-                <p>Descrição</p>
+                <h3>{currentUser.name}</h3>
+                <span>{currentUser.login}</span>
+                <p>{currentUser.bio}</p>
               </div>
-            </div>  
+            </div>
+            <hr />
+          </>
+          ) : null}
 
-          <hr />
-
-          <div>
-            <h4 className='repositorio'>Repositórios</h4>
-            <ItemList title="Teste" description="testes description" />
-            <ItemList title="Teste" description="testes description" />
-            <ItemList title="Teste" description="testes description" />
-            <ItemList title="Teste" description="testes description" />
-          </div>
+          {repos?.length ? (
+            <div>
+              <h4 className='repositorio'>Repositórios</h4>
+              {repos.map(repo => (
+                <ItemList title={repo.name} description={repo.description} />
+              ))}
+            </div>
+          ) : null}
 
         </div>
       </div>
